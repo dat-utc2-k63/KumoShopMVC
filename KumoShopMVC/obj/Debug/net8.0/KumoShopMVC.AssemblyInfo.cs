b2da97bace418ab9e63acc95fbfd01d7b1aa075c
Nguyen Huy <nghuy29032004@gmail.com>
2024-12-06 20:58:07 +0700

    complete fix in git

--- KumoShopMVC/obj/Debug/net8.0/KumoShopMVC.AssemblyInfo.cs
+++ KumoShopMVC/obj/Debug/net8.0/KumoShopMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KumoShopMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de2043b12696e2574522fa4a9dbe7e3c34d347d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d38e2eeeec442593ad2e1120a76a021c5763a0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("KumoShopMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KumoShopMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
